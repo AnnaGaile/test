911bfc555cbe5ff30c51e5d900bd51d33822c471
AnnaGaile <79445887+AnnaGaile@users.noreply.github.com>
2021-04-12 22:06:32 +0300

    first commit

---
+++ proekts.py
@@ -0,0 +1,43 @@
+b=int(input("Kadu miklu tu velies izcept: Plana milka(1), Gaisiga mikla(2)? - "))
+print("")
+if b==1: 
+      print("Lai izcept planu miklu:") 
+      print(" olas - 1 gabals")
+      print(" sals - 4 g,")
+      print(" augu ella - 20 ml,")
+      print(" milti - 165 g")
+elif b==2: 
+      print("Lai izcept gaisigu miklu:")      
+      print(" sausais Raugs - 10 g,")
+      print(" cukurs - 30 g,")
+      print(" sals - 3.5 g,")
+      print(" augu ella - 40 ml,")
+      print(" milti - 450 g,")
+      print(" udens - 280 ml")
+else:
+      print("error")
+print("")
+print("")
+c=int(input("Kuru picu tu velies izcept: Siera pica(1), Desu pica(2), Vistas pica(3)? - "))
+print("")
+if c==1:        
+      print("Lai izcept Siera picu:")
+      print(" mozzarella siers - 250 g,")
+      print(" parmezana siers - 100 g,")
+      print(" kiploks - 10 g")
+      print(" sviests - 30 g")
+elif c==2:     
+      print("Lai izcept Desu picu:")
+      print(" siers - 100 g,")
+      print(" kupinata desa - 200 g,")
+      print(" tomati - 250 g")
+      print(" tomatu merce - 90 g")
+elif c==3:  
+      print("Lai izcept Vistas picu:")
+      print(" vistas fileja - 330 g,")
+      print(" sipoli - 46 g,")
+      print(" majoneze - 17 g,")
+      print(" augu ella - 13 ml,")
+      print(" siers - 133 g")
+else:
+      print("error")
